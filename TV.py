a32cc552d7e97571556ee5626fdb0ca4e2ac82c0
ESims7 <34346851+ESims7@users.noreply.github.com>
2018-06-01 15:53:12 +0100

    Added list of program buttons

--- TV.py
+++ TV.py
@@ -1,14 +1,37 @@
 import os
+import tkinter
+
+window = tkinter.Tk()
 
 programList = []
 seasonList = []
 episodeList =[]
 program = ""
-downloadsFolder = "C:/Users/edwar/Downloads/"
+downloadsFolder = r"C:\Users\hisg473\Desktop\Test"
+
+def print_word():
+    print("word")
+
+def select_season(program):
+    for folderName in os.listdir(downloadsFolder):
+        if program in folderName:
+            seasonList.append(folderName)
+
+    for i in seasonList:
+        b = tkinter.Button(window, text = i, width = 30, command = print_word())
+        b.pack(side=tkinter.TOP)
+
 
 for fileName in os.listdir(downloadsFolder):
     if "Season" in fileName and fileName.split("Season")[0] not in programList:
         programList.append(fileName.split("Season")[0])
+
+for i in programList:
+    b = tkinter.Button(window, text = i, width = 30, command = select_season(i))
+    b.pack(side=tkinter.TOP)
+
+window.mainloop()
+
 
 for i in range(len(programList)):
     print(str(i + 1) + ": " + programList[i])
